229013c3fb4b8f5f8fcce0e4694e2d0022709bb3
kimjuha <dnfxmfkdbzos@naver.com>
2023-03-04 20:42:20 +0900

    level 1

---
+++ level_1/나누어 떨어지는 숫자 배열.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = Arrays.stream(arr).filter(i -> i % divisor == 0).sorted().toArray();
+        if (answer.length == 0)
+            answer = new int[] { -1 };
+        return answer;
+    }
+}
