09596d270a43b5ea73def0d53bfee47478d63c13
kimjuha <dnfxmfkdbzos@naver.com>
2023-03-12 02:22:55 +0900

    level 1

---
+++ level_1/서울에서 김서방 찾기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int i;
+        for (i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim"))
+                break;
+        }
+        return String.format("김서방은 %d에 있다", i);
+    }
+}
