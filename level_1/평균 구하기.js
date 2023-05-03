03d8c6a40791186926ffc49d6cf585878fbc973a
hayamaster <dnfxmfkdbzos@naver.com>
2023-05-04 00:03:40 +0900

    level 1

--- level_1/평균 구하기.js
+++ level_1/평균 구하기.js
@@ -1,5 +1,1 @@
-function solution(arr) {
-  let add = 0;
-  for (let i = 0; i < arr.length; i++) add += arr[i];
-  return add / arr.length;
-}
+const solution = (arr) => arr.reduce((acc, cur) => (acc += cur)) / arr.length;
