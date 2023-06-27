e994162e6a3132936ca439749908105b525d22b7
hayamaster <dnfxmfkdbzos@naver.com>
2023-06-27 15:51:48 +0900

    level 0

---
+++ level_0/구슬을 나누는 경우의 수.js
@@ -0,0 +1,9 @@
+const solution = (balls, share) => {
+  let numerator = 1;
+  let denominator = 1;
+  for (let i = 0; i < share; i++) {
+    numerator *= balls - i;
+    denominator *= share - i;
+  }
+  return numerator / denominator;
+};
