dd94f65971e861ec49576f8f87c0eb4d7b6f5c98
hayamaster <dnfxmfkdbzos@naver.com>
2023-08-01 19:46:27 +0900

    level 2

---
+++ level_2/주차 요금 계산.js
@@ -0,0 +1,45 @@
+const solution = (fees, records) => {
+  const [baseTime, baseFee, unitTime, unitFee] = fees;
+  let recordClone = [...records];
+  let map = new Map();
+  let dic = {};
+  let answer = [];
+
+  records.forEach((info, idx) => {
+    const [time, carNum, InAndOut] = info.split(" ");
+    if (InAndOut == "IN") {
+      map.set(carNum, changeTimes(time));
+    } else {
+      if (!dic[carNum]) {
+        dic[carNum] = changeTimes(time) - map.get(carNum);
+      } else {
+        dic[carNum] += changeTimes(time) - map.get(carNum);
+      }
+      map.delete(carNum);
+    }
+  });
+  for (const [carNum, time] of map.entries()) {
+    if (!dic[carNum]) {
+      dic[carNum] = 24 * 60 - 1 - time;
+    } else {
+      dic[carNum] += 24 * 60 - 1 - time;
+    }
+  }
+
+  let arr = Object.keys(dic).sort((a, b) => a - b);
+  arr = arr.map((key) => dic[key]);
+  for (const time of arr) {
+    answer.push(
+      baseFee +
+        (time > baseTime
+          ? Math.ceil((time - baseTime) / unitTime) * unitFee
+          : 0)
+    );
+  }
+  return answer;
+};
+
+const changeTimes = (time) => {
+  const [hour, min] = time.split(":");
+  return parseInt(hour) * 60 + parseInt(min);
+};
