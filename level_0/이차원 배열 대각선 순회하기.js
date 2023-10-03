7b332809e6dd8abe9498b150fc7f5f12fffabeb5
hayamaster <dnfxmfkdbzos@naver.com>
2023-10-03 23:19:15 +0900

    level 0

---
+++ level_0/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,10 @@
+const solution = (board, k) => {
+  let answer = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (i + j > k) break;
+      answer += board[i][j];
+    }
+  }
+  return answer;
+};
