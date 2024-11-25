1248b3c9c0fcf690feaefcb1fb9a4f8883a87f62
hayamaster <dnfxmfkdbzos@naver.com>
2024-11-24 23:12:32 -0500

    level 0

---
+++ level_0/리스트 자르기.js
@@ -0,0 +1,15 @@
+const solution = (n, slicer, num_list) => {
+  const [a, b, c] = slicer;
+
+  if (n === 1) {
+    return num_list.slice(0, b + 1);
+  }
+  if (n === 2) {
+    return num_list.slice(a, num_list.length);
+  }
+  if (n === 3) {
+    return num_list.slice(a, b + 1);
+  }
+
+  return num_list.slice(a, b + 1).filter((_, idx) => idx % c === 0);
+};
